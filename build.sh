6c002d682991cfc37aca7bbc8efe9e621f0b1f52
yilles <shuwei73.mail@gmail.com>
2025-02-12 18:02:19 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -O nvim-linux64.tar.gz
 sudo tar xzf nvim-linux64.tar.gz -C /opt
 sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
-rm nvim-linux64.tar.gz
 
 wget https://github.com/zellij-org/zellij/releases/download/v0.41.2/zellij-x86_64-unknown-linux-musl.tar.gz -O zellij-x86_64-unknown-linux-musl.tar.gz
 sudo tar xzf zellij-x86_64-unknown-linux-musl.tar.gz -C /opt
 sudo ln -sf /opt/zellij /usr/local/bin/zellij
-rm zellij-x86_64-unknown-linux-musl.tar.gz
 
-ln -sf ~/.dotfiles/nvim/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/.dotfiles/zellij/.config/zellij/config.kdl ~/.config/zellij/config.kdl
-ln -sf ~/.dotfiles/zellij/.config/zellij/config.kdl.bak ~/.config/zellij/config.kdl.bak
+ln -sf ~/dotfiles/nvim/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/zellij/.config/zellij/config.kdl ~/.config/zellij/config.kdl
+ln -sf ~/dotfiles/zellij/.config/zellij/config.kdl.bak ~/.config/zellij/config.kdl.bak
 git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.config/nvim/pack/plugins/start/vim-polyglot
+
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
