0b2af40cefeda18129d67db8caf908fadc7a6265
yilles <shuwei73.mail@gmail.com>
2025-02-14 10:55:11 +0800

    node v24 to v22LTS

--- bash/.bashrc
+++ bash/.bashrc
@@ -10,5 +10,5 @@ function parse_git_branch {
 export PS1="\[\e[32m\]\w\$(parse_git_branch)\[\e[m\] \[\e[31m\]»\[\e[m\] "
 export IP=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
 export PATH=$PATH:/usr/local/bin/nvim-linux-x86_64/bin
-export PATH=$PATH:/usr/local/bin/node-v23.8.0-linux-x64/bin
+export PATH=$PATH:/usr/local/bin/node-v22.14.0-linux-x64/bin
 export PATH=$PATH:/usr/local/bin/go/bin
--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo tar xzf nvim-linux-x86_64.tar.gz -C /usr/local/bin
 wget https://github.com/zellij-org/zellij/releases/download/v0.41.2/zellij-x86_64-unknown-linux-musl.tar.gz -O zellij-x86_64-unknown-linux-musl.tar.gz
 sudo tar xzf zellij-x86_64-unknown-linux-musl.tar.gz -C /usr/local/bin
 
-wget https://nodejs.org/download/release/latest/node-v23.8.0-linux-x64.tar.gz -O node-v23.8.0-linux-x64.tar.gz
-sudo tar xzf node-v23.8.0-linux-x64.tar.gz -C /usr/local/bin
+wget https://nodejs.org/download/release/v22.14.0/node-v22.14.0-linux-x64.tar.gz -O node-v22.14.0-linux-x64.tar.gz
+sudo tar xzf node-v22.14.0-linux-x64.tar.gz -C /usr/local/bin
 
 wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz -O go1.24.0.linux-amd64.tar.gz
 sudo tar xzf go1.24.0.linux-amd64.tar.gz -C /usr/local/bin
